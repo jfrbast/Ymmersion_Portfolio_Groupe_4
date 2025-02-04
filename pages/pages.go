720755e8e73804804ebd0c3dde4cda1e9e859d72
LilianLPV <lilian.lepiver@ynov.com>
2025-02-04 11:20:37 +0100

    feat : implementation page

--- pages/pages.go
+++ pages/pages.go
@@ -15,3 +15,13 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Erreur interne du serveur", http.StatusInternalServerError)
 	}
 }
+
+func TableauDeBord(w http.ResponseWriter, r *http.Request) {
+
+	err := templates.Tpl.ExecuteTemplate(w, "tableaudebord", nil)
+	if err != nil {
+		log.Println("Erreur lors de l'exécution du template :", err)
+
+		http.Error(w, "Erreur interne du serveur", http.StatusInternalServerError)
+	}
+}

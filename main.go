76ab6118624c95f2b4d4755723540b7f398521d9
Thibaut Messina-P <thibaut.messinapaulmeyer@ynov.com>
2025-02-06 09:15:54 +0100

    test

--- main.go
+++ main.go
@@ -13,11 +13,11 @@ func main() {
 	templates.InitTemplates()
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
-
-	http.HandleFunc("/", pages.HomePage)
+	http.HandleFunc("/defi2", pages.Defi2)
 	http.HandleFunc("/portfabio", pages.Portfabio)
 	http.HandleFunc("/dashboard", pages.TableauDeBord)
 	http.HandleFunc("/team", pages.Team)
+	http.HandleFunc("/", pages.HomePage)
 
 	fmt.Println("Serveur démarré sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))

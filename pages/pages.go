c0dd195b644dca076d5665b05c085bd9a9f2f269
Thibaut Messina-P <thibaut.messinapaulmeyer@ynov.com>
2025-02-06 16:33:47 +0100

    update

--- pages/pages.go
+++ pages/pages.go
@@ -52,11 +52,12 @@ func Defi2(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-
 func Defi1(w http.ResponseWriter, r *http.Request) {
 	err := templates.Tpl.ExecuteTemplate(w, "defi_1", nil)
 	if err != nil {
 		log.Println("Erreur lors de l'écution du template :", err)
+	}
+}
 
 // Page "All-defis"
 func Alldefis(w http.ResponseWriter, r *http.Request) {
@@ -64,7 +65,6 @@ func Alldefis(w http.ResponseWriter, r *http.Request) {
 	err := templates.Tpl.ExecuteTemplate(w, "alldefis", nil)
 	if err != nil {
 		log.Println("Erreur lors de l'exécution du template :", err)
-
 
 		http.Error(w, "Erreur interne du serveur", http.StatusInternalServerError)
 	}
